4576d5f3a1cde607c0d2391467bcfafb19c8e09d
K3YB04RD-w4rri0r <francois.saintjean04@gmail.com>
2026-06-01 11:06:27 +0200

    added bottleneck block logic

--- network.py
+++ network.py
@@ -58,29 +58,29 @@ class DEEPCNN(nn.Module):
         
 
 class BasicResBlock(nn.Module):
-    def __init__(self, in_channels : int, out_channels , stride : int = 1, expansion : int = 1):
+    def __init__(self, in_channels : int, red_channels  : int, stride : int = 1, expansion : int = 1):
         super().__init__()
-        self.normalization_fn1 = nn.BatchNorm2d(num_features=out_channels)
-        self.normalization_fn2 = nn.BatchNorm2d(num_features=out_channels * expansion)
+        self.normalization_fn1 = nn.BatchNorm2d(num_features=red_channels)
+        self.normalization_fn2 = nn.BatchNorm2d(num_features=red_channels * expansion)
         self.relu = nn.ReLU()
 
         self.in_channels = in_channels
-        self.out_channels = out_channels
+        self.red_channels = red_channels
         self.expansion = expansion
         self.stride = stride
 
 
 
-        self.layer1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels
+        self.layer1 = nn.Conv2d(in_channels=in_channels, out_channels=red_channels
                                 , kernel_size=3, padding = 3 // 2, padding_mode="zeros")
         
-        if self.in_channels != self.out_channels or self.expansion != 1 or self.stride != 1:
-            self.shortcut = nn.Conv2d(in_channels=in_channels, out_channels=out_channels * expansion
+        if self.in_channels != self.red_channels * self.expansion or self.stride != 1:
+            self.shortcut = nn.Conv2d(in_channels=in_channels, out_channels=red_channels * expansion
                                     , kernel_size=1, padding = 1 // 2, padding_mode="zeros", stride = stride)
         else:
             self.shortcut = nn.Identity()
 
-        self.layer2 = nn.Conv2d(in_channels=out_channels * expansion, out_channels=out_channels * expansion
+        self.layer2 = nn.Conv2d(in_channels=red_channels, out_channels=red_channels * expansion
                                 , kernel_size=3, padding = 3 // 2, padding_mode="zeros", stride = stride)
         
 
@@ -99,9 +99,67 @@ class BasicResBlock(nn.Module):
         return x
     
 
+class BottleneckBlock(nn.Module):
+    def __init__(self, in_channels : int, red_channels : int, expansion : int = 1, stride : int = 1):
+        super().__init__()
+        self.normalization_fn1 = nn.BatchNorm2d(num_features=red_channels)
+        self.normalization_fn2 = nn.BatchNorm2d(num_features=red_channels)
+        self.normalization_fn3 = nn.BatchNorm2d(num_features=red_channels * expansion)
+        self.relu = nn.ReLU()
+
+        self.in_channels = in_channels
+        self.red_channels = red_channels
+        self.expansion = expansion
+        self.stride = stride
+
+        self.layer1 = nn.Conv2d(in_channels=in_channels, out_channels=red_channels
+                                , kernel_size=1, padding = 1 // 2, padding_mode="zeros")
+        
+        if self.in_channels != self.red_channels * expansion or self.stride != 1:
+            self.shortcut = nn.Conv2d(in_channels=in_channels, out_channels=red_channels * expansion
+                                    , kernel_size=1, padding = 1 // 2, padding_mode="zeros", stride = stride)
+        else:
+            self.shortcut = nn.Identity()
+
+        self.layer2 = nn.Conv2d(in_channels=red_channels, out_channels=red_channels
+                                , kernel_size=3, padding = 3 // 2, padding_mode="zeros", stride = stride)
+        
+        self.layer3 =  nn.Conv2d(in_channels=red_channels, out_channels=red_channels * expansion
+                                , kernel_size=1, padding = 1 // 2, padding_mode="zeros")
+    
+
+    def forward(self, x : torch.Tensor):
+
+        skip = self.shortcut(x)
+
+
+        x = self.layer1(x)
+        x = self.normalization_fn1(x)
+        x = self.relu(x)
+        x = self.layer2(x)
+        x = self.normalization_fn2(x) 
+        x = self.relu(x)
+        x = self.layer3(x)
+        x = self.normalization_fn3(x) + skip
+        x = self.relu(x)
+
+        return x
+    
+    
+
+def parameter_count(model : nn.Module):
+    trainable_params = 0
+    for name, p in model.named_parameters():
+        trainable_params += p.numel() 
+    return trainable_params
 
 if __name__ == "__main__":
     x = torch.randn(size = (5,64,56,56))
-    block = BasicResBlock(in_channels=64, out_channels = 5, stride = 2, expansion=5)
-    y = block(x)
-    assert x.shape == y.shape
+    basic_block = BasicResBlock(in_channels=64, red_channels=32, stride = 1, expansion=2)
+    bottleneck_block = BottleneckBlock(in_channels=64, red_channels = 32, stride = 1, expansion=2)
+    print(parameter_count(bottleneck_block))
+    print(parameter_count(basic_block))
+    z = basic_block(x)
+    y = bottleneck_block(x)
+    assert x.shape == y.shape, "x and y don't match"
+    assert x.shape == z.shape, "x and z don't match"
